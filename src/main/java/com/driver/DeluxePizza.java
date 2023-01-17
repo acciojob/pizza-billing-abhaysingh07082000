e5bc5f741a6af3316a42e560dd6ffed7309e6b2e
Abhay Singh <abhaysingh07082000@gmail.com>
2023-01-17 16:09:50 +0530

    pizza billing

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -2,8 +2,23 @@ package com.driver;
 
 public class DeluxePizza extends Pizza {
 
+
+
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
         // your code goes here
+
+
+        this.addTakeaway();
+        this.addExtraCheese();
+        this.addExtraToppings();
+
+
     }
+
+      public int getPrice()
+      {
+          return 300;
+      }
+
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -9,26 +9,27 @@ public class Pizza {
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
-    }
-
-    public int getPrice(){
-        return this.price;
+        this.price=300;
     }
 
     public void addExtraCheese(){
         // your code goes here
+        price=price+80;
     }
 
     public void addExtraToppings(){
         // your code goes here
+        price=price+70;
     }
 
     public void addTakeaway(){
         // your code goes here
+        price=price+20;
     }
 
     public String getBill(){
         // your code goes here
+        bill=String.valueOf(price);
         return this.bill;
     }
 }
